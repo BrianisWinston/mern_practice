f432d613454a4cb75505a44a07e09bb248e39d96
Winston Galas <BrianisWinston@yahoo.com>
2018-09-28 00:52:24 -0700

    get home page route running

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get("/", (req, asd) => asd.send('whas good lil mama lemme whisper in yo ear'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => `aye we lit on ${port} fam`);
+console.log(port);
