368bfa10a5824003e9cd11ccc1f959eb08a182a8
Winston Galas <BrianisWinston@yahoo.com>
2018-09-28 14:57:05 -0700

    Create login user logic in user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
 const User = require('../../models/User.js');
 
 router.get('/test', (req, res) => res.send('In users page.'));
@@ -26,6 +27,27 @@ router.post('/register', (req, res) => {
           });
         });
       };
+    });
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: "Success"});
+          } else {
+            return res.status(400).json({ password: 'Wrong password'})
+          };
+        });
     });
 });
 
