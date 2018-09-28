ae82850eb7e3d07f0f59634d4e16bf44ae40c914
Winston Galas <BrianisWinston@yahoo.com>
2018-09-28 01:51:15 -0700

    connect mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('mongo connected successfully'))
+  .catch( err => console.log(err));
 
 app.get("/", (req, asd) => asd.send('whas good lil mama lemme whisper in yo ear'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => `aye we lit on ${port} fam`);
+app.listen(port, () => console.log(`aye we lit on ${port} fam`));
 console.log(port);
