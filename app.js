04370b597a50cdc00cdd975fa8b610c14d87205c
Winston Galas <BrianisWinston@yahoo.com>
2018-09-28 13:40:01 -0700

    Create user model and schema

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const db = require('./config/keys').mongoURI;
+
+// Import body parser to parse JSON sent to frontend.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Import routes.
 const users = require('./routes/api/users');
 const events = require('./routes/api/events')
 
-
 // Connect project to MongoDB.
+const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db)
   .then(() => console.log('mongo connected successfully'))
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => res.json('in events page'))
+router.get('/test', (req, res) => res.json({ shikamaru: "ninja", naruto: "ninja" }))
 
 module.exports = router;
