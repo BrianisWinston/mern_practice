3a04d5032f24752d2a12dd1aac19ee53c39fc1ed
Winston Galas <BrianisWinston@yahoo.com>
2018-09-29 13:42:27 -0700

    Create event model

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Event = require('../../models/Event');
+const validateEventInput = require('../../validation/events');
 
 router.get('/test', (req, res) => res.json({ shikamaru: "ninja", naruto: "ninja" }))
+
+
 
 module.exports = router;
